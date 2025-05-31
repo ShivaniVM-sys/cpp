57ddf4332b3f2db5dfd47bc06fc87e4b78027f8d
ShivaniVM-sys <shettyshivani16@gmail.com>
2025-05-31 16:08:01 +0530

    day 10

---
+++ CSC.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ServiceCentre 
+{
+    string shopName;
+    int year;
+    string ownerName;
+
+public:
+    void inputShopDetails()
+    {
+        cout << "Enter the name of Common Service Centre: ";
+        getline(cin, shopName);
+
+        cout << "Enter the year shop was opened: ";
+        cin >> year;
+
+        cout << "Enter the name of owner:";
+        getline(cin, ownerName);
+    }
+
+    void displayShopDetails() {
+        cout << "\n--- Welcome to " << shopName << " ---\n";
+        cout << "Opened in: " << year << "\n";
+        cout << "Owner is" << ownerName << "\n";
+
+    }
+
+    void showServices() 
+    {
+        cout << "\nAvailable Services:\n";
+        cout << " Aadhar Update\n";
+        cout << " PAN Card Application\n";
+        cout << " Passport Registration\n";
+        cout << " Ration Card Application\n";
+        cout << "Enter your choice : ";
+    }
+
+    void processService(int choice) {
+        switch (choice) {
+            case 1:
+                cout << "Processing Aadhar Update...\n";
+                break;
+            case 2:
+                cout << "Processing PAN Card Application.\n";
+                break;
+            case 3:
+                cout << "Processing Passport Registration.\n";
+                break;
+            case 4:
+                cout << "Processing Ration Card Application.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please select a valid service.\n";
+        }
+    }
+};
+
+int main() 
+{
+    ServiceCentre csc;
+
+    int choice;
+
+    csc.inputShopDetails();
+    csc.displayShopDetails();
+    csc.showServices();
+    cin >> choice;
+
+    csc.processService(choice);
+
+    return 0;
+}
+
+
+
